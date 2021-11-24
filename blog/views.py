d752506d5ccce57a1d0ee0591de9451eabbdb247
saburay32 <saburay32@gmail.com>
2021-11-24 08:45:25 +0300

    добавляю шаблоны

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+news = [
+    {
+        'title': 'First news',
+        'text': 'Some text',
+        'date': '23.11.2021',
+        'autor': 'Georgiy'
+
+    },
+    {
+        'title': 'Second news',
+        'text': 'Some big 2 text',
+        'date': '23.11.2021'
+
+    }
+]
+
+def home(request):
+    data = {
+        'news': news,
+        'title':'Главная страница блога'
+    }
+    return render(request,'blog/home.html', data)
+
+def contacts(request):
+    return render(request,'blog/contacts.html',{'title': 'О нас'})

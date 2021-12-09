3d3bafbaaaf3c5080792ea609f7f00824f009fee
saburay32 <saburay32@gmail.com>
2021-12-09 08:53:46 +0300

    Добавляю авторизацию и продолжаю с регистрацией

--- site_django/urls.py
+++ site_django/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as userViews
+from django.contrib.auth import views as authViews
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reg/', userViews.register, name='reg'),
+    path('user/', authViews.LoginView.as_view(template_name="users/user.html"), name='user'),
+    path('exit/', authViews.LogoutView.as_view(template_name="users/exit.html"), name='exit'),
     path('', include('blog.urls'))
 ]
